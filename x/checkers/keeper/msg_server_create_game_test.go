29b924c1ec643ee4186dff35d36633b3e7bf7292
Raffo <raffo@pop-os.localdomain>
2023-01-11 11:54:38 +0100

    added some test to create game

--- x/checkers/keeper/msg_server_create_game_test.go
+++ x/checkers/keeper/msg_server_create_game_test.go
@@ -1,14 +1,20 @@
 package keeper_test
 
 import (
+	"context"
 	"testing"
 
+	keepertest "github.com/alice/checkers/testutil/keeper"
+	"github.com/alice/checkers/x/checkers"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/alice/checkers/x/checkers/keeper"
 	"github.com/alice/checkers/x/checkers/types"
 	"github.com/stretchr/testify/require"
 )
 
 func TestCreateGame(t *testing.T) {
-	msgServer, context := setupMsgServer(t)
+	msgServer, _, context := setupMsgServerCreateGame(t)
 	createResponse, err := msgServer.CreateGame(context, &types.MsgCreateGame{
 		Creator: alice,
 		Black:   bob,
@@ -17,7 +23,95 @@ func TestCreateGame(t *testing.T) {
 
 	require.Nil(t, err)
 	require.EqualValues(t, types.MsgCreateGameResponse{
-		GameIndex: "",
+		GameIndex: "1",
 	},
 		*createResponse)
+}
+
+func setupMsgServerCreateGame(t testing.TB) (types.MsgServer, keeper.Keeper, context.Context) {
+	k, ctx := keepertest.CheckersKeeper(t)
+	checkers.InitGenesis(ctx, *k, *types.DefaultGenesis())
+	return keeper.NewMsgServerImpl(*k), *k, sdk.WrapSDKContext(ctx)
+}
+
+func TestCreate1GameHasSaved(t *testing.T) {
+	msgSrvr, keeper, context := setupMsgServerCreateGame(t)
+	msgSrvr.CreateGame(context, &types.MsgCreateGame{
+		Creator: alice,
+		Black:   bob,
+		Red:     carol,
+	})
+
+	systemInfo, found := keeper.GetSystemInfo(sdk.UnwrapSDKContext(context))
+	require.True(t, found)
+	require.EqualValues(t, types.SystemInfo{
+		NextId: 2,
+	}, systemInfo)
+
+	game1, found1 := keeper.GetStoredGame(sdk.UnwrapSDKContext(context), "1")
+
+	require.True(t, found1)
+	require.EqualValues(t, types.StoredGame{
+		Index: "1",
+		Board: "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:  "b",
+		Black: bob,
+		Red:   carol,
+	}, game1)
+
+}
+
+func TestCreate3Game(t *testing.T) {
+	msgServer, _, context := setupMsgServerCreateGame(t)
+
+	createResponse, err := msgServer.CreateGame(context, &types.MsgCreateGame{
+		Creator: alice,
+		Black:   bob,
+		Red:     carol,
+	})
+	require.Nil(t, err)
+	require.EqualValues(t, types.MsgCreateGameResponse{
+		GameIndex: "1",
+	}, *createResponse)
+
+	createResponse2, err2 := msgServer.CreateGame(context, &types.MsgCreateGame{
+		Creator: alice,
+		Black:   bob,
+		Red:     carol,
+	})
+	require.Nil(t, err2)
+	require.EqualValues(t, types.MsgCreateGameResponse{
+		GameIndex: "2",
+	}, *createResponse2)
+
+}
+
+func TestCreateGameRedAddressBad(t *testing.T) {
+	msgServer, _, context := setupMsgServerCreateGame(t)
+
+	createResponse, err := msgServer.CreateGame(context, &types.MsgCreateGame{
+		Creator: alice,
+		Black:   bob,
+		Red:     "badaddress",
+	})
+	require.Nil(t, createResponse)
+	require.NotNil(t, err)
+	require.Equal(t,
+		"red address is invalid: badaddress: decoding bech32 failed: invalid separator index -1",
+		err.Error())
+}
+
+func TestCreateGameEmptyAddress(t *testing.T) {
+	msgServer, _, context := setupMsgServerCreateGame(t)
+
+	createResponse, err := msgServer.CreateGame(context, &types.MsgCreateGame{
+		Creator: alice,
+		Black:   bob,
+		Red:     "",
+	})
+	require.Nil(t, createResponse)
+	require.NotNil(t, err)
+	require.Equal(t,
+		"red address is invalid: : empty address string is not allowed",
+		err.Error())
 }
